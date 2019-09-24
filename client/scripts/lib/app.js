742cd1fc5b155a5e215b9b7285d314369b3e2f1b
Zack Claar <claar.zack@gmail.com>
2019-09-24 08:17:07 -0600

    Add route config to main app.js file. Create template for multiple chat tabs

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -4,8 +4,14 @@ import 'angular-meteor';
 import 'angular-sanitize';
 import 'angular-ui-router';
 import 'ionic-scripts';
+
+
 import Angular from 'angular';
+import Loader from 'angular-ecmascript/module-loader';
 import { Meteor } from 'meteor/meteor';
+
+// Routes config
+import RoutesConfig from '../routes';
 
 // Modules to add
 const App = 'Whatsapp';
@@ -15,6 +21,10 @@ Angular.module(App, [
     'angular-meteor',
     'ionic'
 ]);
+
+// App loader
+new Loader(App)
+  .load(RoutesConfig);
 
 // App startup
 if (Meteor.isCordova) {
